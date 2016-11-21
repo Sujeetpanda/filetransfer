e92a58e9a7424331d528aba36abd6b74ebb8b3b7
Sujeetpanda <sujeet1608@gmail.com>
2016-11-22 01:07:14 +0530

    constants

---
+++ src/main/java/com/torch/utility/constants/FileTransferConstants.java
@@ -0,0 +1,13 @@
+package com.torch.utility.constants;
+
+public class FileTransferConstants {
+	public static final String SFTP = "sftp";
+	public static final String SCP = "scp";
+	public static final String EXEC = "exec";
+	public static final String INVALID_DETAILS = "Please provide the valid username,password and host";
+	public static final String FAILED_TO_CREATE_CHANNEL = "Failed to create channel";
+	public static final String UNABLE_TO_OPEN_CHANNEL = "Unable to open channel";
+	public static final String FILE_UPLOAD_SUCCESS = "Successfully uploaded the file";
+	public static final String FILE_UPLOAD_FAILED = "Unable to upload the file";
+	public static final int PPORT = 22;
+}
